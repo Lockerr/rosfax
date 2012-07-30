120ee06b56769726a21a9ecb6bdb036e5f3a3b53
renius <antiqe@gmail.com>
2012-07-30 17:08:17 +0600

    assets

--- app/models/report.rb
+++ app/models/report.rb
@@ -137,7 +137,7 @@ class Report < ActiveRecord::Base
         if collection.any?
           images[place] = [Asset.find(collection.first).url(:thumb), collection.size]
         else
-          images[place] = [asset_path('images/box.png'), 0]
+          images[place] = [asset_path('box.png'), 0]
       end
     end
 

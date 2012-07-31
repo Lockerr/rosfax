24dd07d0cff7d0800a4957b91948daa33e164134
renius <antiqe@gmail.com>
2012-07-31 13:01:31 +0600

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tradein::Application.initialize!
+
+config.after_initialize do
+  Delayed::Job.scaler = :null
+end

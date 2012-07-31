299d4005916ad0eadc6f06bae30fb80f262ce3f7
renius <antiqe@gmail.com>
2012-07-31 10:26:34 +0600

    s3 fix

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,12 +1,12 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :data, :styles => {
-          :thumb => '100x68',
-          :carousel => '900x600',
-          },
-           :storage => 's3',
-           :s3_credentials => "#{Rails.root}/config/s3.yml",
-           :path => ":attachment/:id/:style.:extension",
-           :s3_host_name =>  's3-website-eu-west-1.amazonaws.com'
+  has_attached_file :data,
+          :styles => {:thumb => '100x68', :carousel => '900x600'},
+          :storage => :s3,
+          :s3_credentials => { :access_key_id => 'AKIAJVTSIEA4Y2WZG5TQ', :secret_access_key => '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' },
+          :bucket => 'rosfax',
+          :path => ":attachment/:id/:style.:extension",
+          :s3_host_name =>  's3-eu-west-1.amazonaws.com'
+
 
   process_in_background :data
 

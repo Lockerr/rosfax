a46bafaf6b1d7ea3a698c9affa8211be1b97ae41
renius <antiqe@gmail.com>
2012-08-01 11:15:41 +0600

    unicorn gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,10 +54,10 @@ end
 namespace :deploy do
 
   task :restart do
-    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D -p 83; fi"
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D ; fi"
   end
   task :start do
-    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D -p 83"
+    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
   end
   task :stop do
     run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"

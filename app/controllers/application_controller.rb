d5ad8d3fd9cb318abf0f6871d2637427fb76e0ce
renius <antiqe@gmail.com>
2012-08-07 16:31:41 +0600

    assets fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  def index
-    warn params[:key].force_encoding("UTF-8").blank?
-  end
+  # def index
+  #   warn params[:key].force_encoding("UTF-8").blank?
+  # end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,13 +1,12 @@
 class Asset < ActiveRecord::Base
   has_attached_file :data,
           :styles => {:thumb => '100x68', :carousel => '900x600'},
-          :default_url => "asset_path('loading.gif')",
           :storage => :s3,
           :s3_credentials => { :access_key_id => 'AKIAJVTSIEA4Y2WZG5TQ', :secret_access_key => '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' },
           :bucket => 'rosfax',
           :path => ":attachment/:id/:style.:extension",
           :s3_host_name =>  's3-eu-west-1.amazonaws.com',
-          :default_url => "/images/normal/missing.png"
+          :default_url => "/assets/loading.gif"
 
 
   process_in_background :data

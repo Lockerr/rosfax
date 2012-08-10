774c897730e56d11b4424defd9a6c4a3f543dcab
renius <antiqe@gmail.com>
2012-08-10 16:58:02 +0600

    fix

--- app/controllers/ftp_controller.rb
+++ app/controllers/ftp_controller.rb
@@ -1,11 +1,11 @@
 class FtpController < ApplicationController
   def file
-    user = User.find_by_email(params[:file].split(/\//)[4].gsub(/\\@/, '@')
+    user = User.find_by_email(params[:file].split(/\//)[4])
 
     Rails.logger.warn params[:file].split(/\//)[4].inspect
     if user
 
-      file = File.new(params[:file])
+      file = File.new(params[:file].gsub('@', '\\@')
 
       asset = user.assets.new :data => file
       asset.save

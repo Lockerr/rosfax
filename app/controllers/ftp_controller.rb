f855b914d2afd2a48a112d5886fc1397194a6f07
renius <antiqe@gmail.com>
2012-08-10 17:40:35 +0600

    fix

--- app/controllers/ftp_controller.rb
+++ app/controllers/ftp_controller.rb
@@ -2,6 +2,8 @@ class FtpController < ApplicationController
   def file
     user = User.find_by_email(params[:file].split(/\//)[4].gsub(/\\@/, '@'))
 
+    Rails.logger.warn "User: user.email" if user
+
     Rails.logger.warn params[:file].split(/\//)[4].inspect
     if user
 

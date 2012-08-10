33d255014bb87eab0dc59cc6f544b911a7c8a41e
renius <antiqe@gmail.com>
2012-08-10 14:39:21 +0600

    fix

--- app/controllers/ftp_controller.rb
+++ app/controllers/ftp_controller.rb
@@ -3,7 +3,8 @@ class FtpController < ApplicationController
     user = User.find_by_email(params[:file].split(/\//)[4]))
     if user
       file = params[:file]
-      user.assets.create :data => File.new file, 'r'
+      user.assets.create :data => File.new( file, 'r' )
+
     else
       Rails.logger.warn " ===================== params:\n #{params.inspect} \n ========================="
       Rails.logger.warn " ======================= User not found =================================="

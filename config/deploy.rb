4b370434bb85565804e10def6d679127129ccbd9
renius <antiqe@gmail.com>
2012-08-22 15:34:14 +0600

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 #require 'rvm/capistrano'
 #require 'bundler/capistrano'
 # Add RVM's lib directory to the load path.
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 
 # Load RVM's capistrano plugin.
 require "rvm/capistrano"

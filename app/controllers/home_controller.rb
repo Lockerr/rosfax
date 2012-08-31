a83fbdd87d719a35767a33fd898397b622c971bb
renius <antiqe@gmail.com>
2012-08-31 10:36:15 +0600

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    redirect_to reports_path if Rails.root.to_s.split('/')[2] == 'user'
     @models = {}
     Model.includes(:brand).select(['models.name', 'brands.name', 'models.id']).map {|y| @models[[y.brand.name, y.name].join(' ')] = y.id}.sort
 

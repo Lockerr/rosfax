74ac91eeac450cf1d93522fe745a8df92f2f18e4
renius <antiqe@gmail.com>
2012-09-12 11:23:28 +0600

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
-  def index
+  def partners
     redirect_to reports_path if Rails.root.to_s.split('/')[2] == 'user'
     @models = {}
     Model.includes(:brand).select(['models.name', 'brands.name', 'models.id']).map {|y| @models[[y.brand.name, y.name].join(' ')] = y.id}.sort
 
+  end
+  def index  	
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,8 +40,9 @@ Tradein::Application.routes.draw do
 
   get 'ftp/update_eye_fi'
   
-  root :to => 'home#index', :constraints => {:subdomain => "partners"}
-  root :to => 'home#andex'
+  root :to => 'home#partners', :constraints => {:subdomain => "partners"}
+  root :to => 'home#partners', :constraints => {:subdomain => "partner"}
+  root :to => 'home#index'
 
   match 'new_file', :controller => 'ftp', :action => 'file'
   match 'google5bf74eb79251ba45.html',  :controller => 'static', :action => 'google_validation'

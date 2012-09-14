63eb2478ab0c4d3cd854c8cd6bb95933b5f591d6
renius <antiqe@gmail.com>
2012-09-14 11:38:39 +0600

    color cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,19 @@
 require 'capistrano/ext/multistage'
 require "rvm/capistrano"
 
+
+require 'capistrano_colors'
+
+capistrano_color_matchers = [
+ { :match => /command finished/,       :color => :hide,      :prio => 10 },
+ { :match => /executing command/,      :color => :blue,      :prio => 10, :attribute => :underscore },
+ { :match => /^transaction: commit$/,  :color => :magenta,   :prio => 10, :attribute => :blink },
+ { :match => /git/,                    :color => :white,     :prio => 20, :attribute => :reverse },
+]
+
+colorize( capistrano_color_matchers )
+
+
 set :default_stage, 'staging'
 
 set :application, "rosfax"

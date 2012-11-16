fa5e53b9fef90f39d162238043ae73ead842d9db
renius <antiqe@gmail.com>
2012-11-16 11:33:43 +0600

    fix

---
+++ app/views/feedbacks/create.js
@@ -0,0 +1,3 @@
+$('body').prepend("<div class='alert-feedback alert alert-success' style='position: fixed;top: 40px'>Спасибо, ваше сообщение отправлено</div>");
+$('.modal').modal('hide');
+$('.alert-feedback').fadeOut(10000);
--- app/views/feedbacks/create.js.coffee
+++
@@ -1,3 +0,0 @@
-$('body').prepend("<div class='alert-feedback alert alert-success' style='position: fixed;top: 40px'>Спасибо, ваше сообщение отправлено</div>")
-$('.modal').modal('hide')
-$('.alert-feedback').fadeOut(10000)

ab83a317fc7f5a6786edc5400890dcdee94817ef
renius <antiqe@gmail.com>
2012-11-21 14:00:03 +0600

    remove domains all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Tradein::Application.config.session_store :cookie_store, key: '_tradein_session', :domain => :all
+Tradein::Application.config.session_store :cookie_store, key: '_tradein_session'
 
 
 # Use the database for sessions instead of the cookie-based default,

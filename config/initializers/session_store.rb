588a81d710c3fead4b5d6c220bfb5f19999936e4
renius <antiqe@gmail.com>
2012-11-21 13:50:57 +0600

    dont know

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,7 @@
 
 Tradein::Application.config.session_store :cookie_store, key: '_tradein_session'
 
+
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")

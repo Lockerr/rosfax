50d7b740a43657d94dca977bb6509b0608351cf3
renius <antiqe@gmail.com>
2012-12-03 16:37:04 +0600

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -27,6 +27,8 @@ set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
 namespace :deploy do
+  after :update_code, :assets:precompile
+  
   task :restart do
     run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd /home/perekup/rosfax/current && rvm r328 do bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D ; fi"
   end
@@ -40,4 +42,18 @@ namespace :deploy do
   task :migrate do
     run "cd /home/perekup/rosfax/current && bundle exec rake db:migrate"
   end
+  
+
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+  end
+
 end
+
+ 

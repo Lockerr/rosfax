df58233b9923b7c18beed1ccbe09e9861bfdbe6e
renius <antiqe@gmail.com>
2012-12-04 15:15:04 +0600

    changing report pdf and show  markup

--- app/models/point.rb
+++ app/models/point.rb
@@ -7,7 +7,7 @@ class Point < ActiveRecord::Base
   scope :elements, where(:object => :elements)
   scope :defects, where(:object => :defect)
   scope :checks, where(:object => :check)
-  scope :filled, where('`condition` is not null or `state` is not null or `description` is not null')
+  scope :filled, where('`condition` is not null or `state` is not null')
   
   validates_presence_of :object
   serialize :images, Array

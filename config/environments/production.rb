2d9bdbf0e3dd49f1e2a4735b83d42e117ef5fb17
renius <antiqe@gmail.com>
2012-12-04 18:01:42 +0600

    only precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Tradein::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -44,7 +44,7 @@ Tradein::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( clean.js.coffee home.js.coffee reports.js.coffee reports_show.js.coffee)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

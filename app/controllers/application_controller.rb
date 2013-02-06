5645cb3f474cf3f10418e18fc643b63505966134
renius <antiqe@gmail.com>
2013-02-06 16:57:49 +0600

    locales, caches, cancan, email fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, :alert => exception.message
-  # end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to :back, :notice => exception.message
+  end
 
   def after_sign_out_path_for(resource_or_scope)
     root_path

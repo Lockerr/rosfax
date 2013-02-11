3f248a55acec741e8b2175bf851574bdc123c14b
renius <antiqe@gmail.com>
2013-02-11 11:13:57 +0600

    fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,11 @@
 class StaticController < ApplicationController
   newrelic_ignore
+
   def google_validation
     render :text =>  'google-site-verification: google5bf74eb79251ba45.html'
   end
 
   def yandex_validation
-    render :layout => false
+    render :text => "<html><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'></head><body>Verification: 4ed32e0d73358fa4</body></html>".html_safe
   end
 end

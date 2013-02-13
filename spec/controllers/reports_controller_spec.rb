6a656904392992a6a74ad131ba1b5e41c14619ab
renius <antiqe@gmail.com>
2013-02-13 10:31:17 +0600

    fix

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -24,7 +24,7 @@ describe ReportsController do
   # Report. As you add validations to Report, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {model: Model.create!(name: 'Almera') }
+    {model: Model.create!(name: 'Almera'), publish: true }
   end
 
   # This should return the minimal set of values that should be in the session

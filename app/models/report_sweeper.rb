bf61879a77f4faee0b0cf4cba7c578606c45df6b
renius <antiqe@gmail.com>
2013-02-15 15:10:40 +0600

    fix

--- app/models/report_sweeper.rb
+++ app/models/report_sweeper.rb
@@ -14,8 +14,15 @@ class ReportSweeper < ActionController::Caching::Sweeper
   def expire_cache_for(record)
     object = record if record.is_a?(Report)
     object = record.report if record.is_a?(Point)
-    object = record.report if record.is_a?(Asset)
-    
+    if record.is_a?(Asset)
+      if record.attachable .is_a?(Report)
+        object = record.attachable
+      end
+    end
+
+    return true unless object
+
+
 
     expire_action edit_report_url(object)
     expire_action report_url(object)

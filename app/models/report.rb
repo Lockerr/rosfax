d148292f945c22f03f82722f8cb3f0af1d4b7b5c
renius <antiqe@gmail.com>
2013-04-01 18:33:03 +0600

    xml translations

--- app/models/report.rb
+++ app/models/report.rb
@@ -246,6 +246,7 @@ class Report < ActiveRecord::Base
     points.where(:object => ['checklist', 'elements']).each do |point|
       point_hash = {}
       point_object = point.attributes.except('id', 'report_id', 'created_at', 'updated_at', 'images','assets_count', 'object', 'section')
+      point_object['place'] = I18n.translate("#{point.object}.#{point.section}.#{point.place}").to_s
 
       point_object[:assets] = []
 
